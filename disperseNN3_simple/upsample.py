66547bd69bb7a64bded4116e29bf819f81312e32
chriscrsmith <chriscs@colorado.edu>
2023-04-27 07:27:54 -0700

    good progress

--- disperseNN3_simple/upsample.py
+++ disperseNN3_simple/upsample.py
@@ -13,9 +13,11 @@ scal = 1 # multiply this by the transformed layer sizes
 ds = [[45, 5292], # feature_block1
       [45,4], # locs1
       [10,10], # dense0
-      [100,100], # upsample1
-      #[100, 5296], # feature_block2
-      [(np.sqrt(100)-np.sqrt(45))**2, (np.sqrt(5292)+np.sqrt(4))**2], # padding2
+      [150,150], # upsample1
+      [(np.sqrt(150)-np.sqrt(45))**2, (np.sqrt(5292)+np.sqrt(4))**2], # padding2
+      [500,500], # upsample2
+      [(np.sqrt(500)-np.sqrt(45))**2, (np.sqrt(5292)+np.sqrt(4))**2], # padding3                                                                                                                     
+#      [500,500], # upsample3
       ]
 
 # defined your arch
@@ -36,59 +38,54 @@ arch = [
     to_Dense("upsample1", ds[3][0], ds[3][1], offset="(1,0,0)", to="(dense0-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1),
     to_connection("dense0", "upsample1"),
 
-    # feature block
-    to_Dense("feature_block2", "", "", offset="(1,0,0)", to="(upsample1-east)", height=my_trans(ds[0][1])*scal, depth=my_trans(ds[0][0])*scal, width=1 ),                            
-    to_connection("upsample1", "feature_block2"),                                                                                                                                    
-    to_DensePurp("locs2", ds[1][0], ds[0][1]+ds[1][1], offset="(-0.1,-0.2,0)", to="(feature_block2-south)", height=my_trans(ds[1][1])*scal, depth=my_trans(ds[1][0])*scal, width=1),
-    to_Dense("padding2", "", "", offset="(-0.475, -0.475, -1.75)", to="(feature_block2-east)", height=my_trans(ds[4][1])*scal, depth=my_trans(ds[4][0])*scal, width=1 ), # SUPER painstaking
-    to_Dense("upsample2", ds[3][0], ds[3][1], offset="(-0.475, -8.75, 0)", to="(padding2-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1),
+    ########################### these offsets work great for width=100
+    # # feature block
+    # to_Dense("feature_block2", "", "", offset="(1,0,0)", to="(upsample1-east)", height=my_trans(ds[0][1])*scal, depth=my_trans(ds[0][0])*scal, width=1 ),                            
+    # to_connection("upsample1", "feature_block2"),                                                                                                                                    
+    # to_DensePurp("locs2", ds[1][0], ds[0][1]+ds[1][1], offset="(-0.1,-0.2,0)", to="(feature_block2-south)", height=my_trans(ds[1][1])*scal, depth=my_trans(ds[1][0])*scal, width=1),
+    # to_Dense("padding2", "", "", offset="(-0.475, -0.475, -1.75)", to="(feature_block2-east)", height=my_trans(ds[4][1])*scal, depth=my_trans(ds[4][0])*scal, width=1 ), # SUPER painstaking
+    # to_Dense("concat2", ds[3][0], ds[3][1], offset="(-0.475, -8.75, 0)", to="(padding2-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1),
+    ##########################
+
+    # feature block                                                                                                                                                                         
+    to_Dense("feature_block2", "", "", offset="(1.25,0,0)", to="(upsample1-east)", height=my_trans(ds[0][1])*scal, depth=my_trans(ds[0][0])*scal, width=1 ),                                   
+    to_connection("upsample1", "feature_block2"),                                                                                                                                           
+    to_DensePurp("locs2", ds[1][0], ds[0][1]+ds[1][1], offset="(-0.1,-0.2,0)", to="(feature_block2-south)", height=my_trans(ds[1][1])*scal, depth=my_trans(ds[1][0])*scal, width=1),        
+    to_Dense("padding2", "", "", offset="(-0.41, -0.4, -1.75)", to="(feature_block2-east)", height=my_trans(ds[4][1])*scal, depth=my_trans(ds[4][0])*scal, width=1 ), # SUPER painstaking
+    to_Dense("concat2", ds[3][0], ds[3][1], offset="(-0.475, -9, -0.06)", to="(padding2-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1),                     
+    
+    # map 2                                                                                                                                                                                 
+    to_Dense("dense2", ds[3][0], ds[3][1], offset="(1.5,0,0)", to="(feature_block2-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1),
+    to_connection("feature_block2", "dense2"),
+
+    # upsample                                                                                                                                                                              
+    to_Dense("upsample2", ds[5][0], ds[5][1], offset="(1.75,0,0)", to="(dense2-east)", height=my_trans(ds[5][1])*scal, depth=my_trans(ds[5][0])*scal, width=1),
+    to_connection("dense2", "upsample2"),
+
+    # feature block                                                                                                                                                                         
+    to_Dense("feature_block3", "", "", offset="(1.5,0,0)", to="(upsample2-east)", height=my_trans(ds[0][1])*scal, depth=my_trans(ds[0][0])*scal, width=1 ),
+    to_connection("upsample2", "feature_block3"),
+    to_DensePurp("locs3", ds[1][0], ds[0][1]+ds[1][1], offset="(-0.1,-0.2,0)", to="(feature_block3-south)", height=my_trans(ds[1][1])*scal, depth=my_trans(ds[1][0])*scal, width=1),
+    to_Dense("padding3", "", "", offset="(-0.475, -0.475, -1.75)", to="(feature_block3-east)", height=my_trans(ds[6][1])*scal, depth=my_trans(ds[6][0])*scal, width=1 ), # SUPER painstaking
+    to_Dense("concat3", ds[5][0], ds[5][1], offset="(-0.475, -8.75, 0)", to="(padding3-east)", height=my_trans(ds[5][1])*scal, depth=my_trans(ds[5][0])*scal, width=1),
+
+    # # map 1
+    # to_Dense("dense3", ds[5][0], ds[5][1], offset="(1.5,0,0)", to="(feature_block3-east)", height=my_trans(ds[5][1])*scal, depth=my_trans(ds[5][0])*scal, width=1),
+    # to_connection("feature_block3", "dense3"),
+    
+    # # upsample                                                                                                                                              
+    # to_Dense("upsample3", ds[7][0], ds[7][1], offset="(1.5,0,0)", to="(dense3-east)", height=my_trans(ds[7][1])*scal, depth=my_trans(ds[7][0])*scal, width=1),
+    # to_connection("dense3", "upsample3"),
+
+    # invisible layer for drawing connection
+    to_Dense("invis1", "","", offset="(2,0,0)", to="(feature_block3-east)", height=0, depth=0, width=0),
+    to_connection("feature_block3", "invis1"),
    
-   
     
     
     
     
-#     # input
-#     to_input( 'genos_flip.png', width=1, height=14), # might need jpg...
 
-#     # invisible layer, only for drawing a connection to the input image
-#     to_Conv("input", "", "", offset="(-2.5,0,0)", to="(0,0,0)", height=0, depth=0, width=0 ),
-
-#     # another invisible layer, for getting layer-size labels onto the input image
-#     to_Conv("input_lab", "", ds[1][0], offset="(-3.7,-7.5,0)", to="(0,0,0)", height=0, depth=0, width=0 ),
-#     to_Conv("input_lab", "", ds[1][1], offset="(-2.8,-7.1,0)", to="(0,0,0)", height=0, depth=0, width=0 ),
-
-#     # downsampling/encoding layers
-#     to_Conv("conv0", ds[2][0], ds[2][1], offset="(-1,0,0)", to="(0,0,0)", height=my_trans(ds[2][1])*scal, depth=my_trans(ds[2][0])*scal, width=1 ),
-#     to_connection( "input", "conv0"),
-#     to_Pool("pool0", ds[3][0], ds[3][1], offset="(0.5,0,0)", to="(conv0-east)", height=my_trans(ds[3][1])*scal, depth=my_trans(ds[3][0])*scal, width=1), 
-
-#     to_Conv("conv1", ds[4][0], ds[4][1], offset="(1,0,0)", to="(pool0-east)",height=my_trans(ds[4][1])*scal, depth=my_trans(ds[4][0])*scal, width=1 ),
-#     to_connection( "pool0", "conv1"),
-#     to_Pool("pool1", ds[5][0], ds[5][1], offset="(0.5,0,0)", to="(conv1-east)", height=my_trans(ds[5][1])*scal, depth=my_trans(ds[5][0])*scal, width=1),
-
-#     # to_Dense("dense0", ds[6][0], ds[6][1], offset="(1,0,0)", to="(pool1-east)", height=my_trans(ds[6][1])*scal, depth=my_trans(ds[6][0])*scal, width=1), 
-#     # to_connection("pool1", "dense0"),
-
-    
-#     # # flatten  + densep
-#     # to_Dense("flatten", ds[7][0], ds[7][1], offset="(1,0,0)", to="(dense0-east)", height=my_trans(ds[7][1])*scal, depth=my_trans(ds[7][0])*scal, width=1 ),
-#     # to_connection( "dense0", "flatten"),
-
-#     # locs
-#     to_input( 'locs_flip.png', to='(-3,-9.0,0)', width=4, height=1),
-#     to_DensePurp("locs", "","", offset="(-0.5,-8.6,0)", to="(conv1-south)", height=0, depth=0, width=0),
-#     to_DensePurp("locs2", ds[9][0], ds[9][1], offset="(-0.1,-8.1)", to="(pool1-south)", height=my_trans(ds[9][1])*scal, depth=my_trans(ds[9][0])*scal, width=1),
-# #    to_connection( "locs", "locs2"),
-#     to_Dense("pencil", "", "", offset="(1,0,0)", to="(pool1-east)", height=my_trans(ds[7][1])*scal, depth=my_trans(ds[7][0])*scal, width=1),
-#     to_connection( "pool1", "pencil"),
-#     to_DensePurp("eraser", ds[9][0], ds[7][1]+ds[9][1], offset="(-0.1,-0.2,0)", to="(pencil-south)", height=my_trans(ds[9][1])*scal, depth=my_trans(ds[9][0])*scal, width=1),    
-#     to_connection( "locs2", "eraser"),
-
-#     # invisible layer for some connections
-#     to_Dense("invis1", "","", offset="(1,0,0)", to="(pencil-east)", height=0, depth=0, width=0),
-#     to_connection( "pencil", "invis1"),    
-#     to_Dense("invis2", "","", offset="(4,0,0)", to="(invis1-east)", height=0, depth=0, width=0),
             
 
 
@@ -108,3 +105,4 @@ def main():
 if __name__ == '__main__':
     main()
 
+OA
